5a264b62aa9c91672030a8c942a624c2d6b522e7
Chandrikatammana2002 <95030840+Chandrikatammana2002@users.noreply.github.com>
2023-09-10 16:26:53 +0530

    Update HandleApi.js

--- frontend/src/HandleApi.js
+++ frontend/src/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl="http://localhost:5000";
+const baseUrl="https://energetic-cow-gabardine.cyclic.app/";
 
 const getAllTodo=(setTodo)=>{
     axios.get(baseUrl).then(({data})=>{
